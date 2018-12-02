ad055e05c88da229508dd40ac9377b458c7c0362
aarontbautista <aaronnolbautista@gmail.com>
2018-12-03 02:32:20 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+
+var app = express();
+
+var port = process.env.PORT || 8080
+
+var server= app.listen(port, listening);
+
+function listening() {
+	console.log('listening. . .');
+}
